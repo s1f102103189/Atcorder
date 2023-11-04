07afd878e7b87c6aae7b5831478cfb8fc75978d9
KOYAMA SHOYA <s1f102103189@iniad.org>
2023-11-04 23:39:46 +0900

    Update hhkb.py

--- temp/hhkb.py
+++ temp/hhkb.py
@@ -1,33 +1,61 @@
+# B = int(input())
+# b = False
+
+# for i in range(18):
+#     if i ** i == B:
+#         b = True
+#         if b == True:
+#             print(i)
+# print(-1)
+
 B = int(input())
-b = False
+found = False
 
-for i in range(18):
+for i in range(1, int(B**0.5)+1):  # A^A will not exceed the square root of B
     if i ** i == B:
-        b = True
-        if b == True:
-            print(i)
-print(-1)
+        print(i)
+        found = True
+        break
 
-N = int(input())
-S = input()
+if not found:
+    print(-1)
 
-count = 0
-for i in range(N-1):
-    if (S[i] == 'a') and (S[i+1] == 'b'):
-        print('Yes')
-        count += 1
-    elif (S[i] == 'b') and (S[i+1] == 'a'):
-        print('Yes')
-        count += 1
+def find_integer_power(B):
+    # We start at 1 because 0 to any power will not equal a positive B
+    for A in range(1, B + 1):  # A cannot be larger than B
+        if A**A == B:
+            return A
+    return -1
+
+
+# N = int(input())
+# S = input()
+
+# count = 0
+# for i in range(N-1):
+#     if (S[i] == 'a') and (S[i+1] == 'b'):
+#         print('Yes')
+#         count += 1
+#     elif (S[i] == 'b') and (S[i+1] == 'a'):
+#         print('Yes')
+#         count += 1
     
-if count != 1:
-    print('No')
+# if count != 1:
+#     print('No')
 
+
+# N = int(input())
+# S = input()
+
+# if ("ab" or "ba") in S:
+#     print("Yes")
+# else:
+#     print("No")
 
 N = int(input())
 S = input()
 
-if ("ab" or "ba") in S:
+if "ab" in S or "ba" in S:
     print("Yes")
 else:
     print("No")
